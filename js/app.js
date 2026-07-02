79ffd65a42dc274a8ca9c8d26af067d58074fb0e
umammarshmallow <umammarshmallow@gmail.com>
2026-07-02 20:59:06 +0700

    Initialize Airdrop Hub on DOMContentLoaded

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log("Airdrop Hub berhasil dijalankan!");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Airdrop Hub v1.0");
+
+    document.getElementById("activeProjects").textContent = 0;
+    document.getElementById("wallets").textContent = 0;
+    document.getElementById("todayTasks").textContent = 0;
+    document.getElementById("completed").textContent = 0;
+});!");
